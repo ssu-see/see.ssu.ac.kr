f91cb96ff3b8351cfae5047e6e5b186230907f7a
wikibootup <wikibootup@gmail.com>
2015-02-17 22:21:38 -0500

    Add admin user permission category added

--- seeseehome/users/admin.py
+++ seeseehome/users/admin.py
@@ -3,19 +3,19 @@ from users.models import User
 from django.contrib.auth.models import Group
 from users.forms import UserForm
 
+
 admin.site.unregister(Group)
+
 
 class UserAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
     form = UserForm
-    list_display = ('username', 'email', 'contact_number')
+    list_display = ('username', 'email', 'contact_number', 'userperm')
 #   admin does not have edit following fields
-    exclude = ('password', 'username', 'email', 
-                'last_login', 'contact_number')
+    exclude = ('password', 'username', 'email',
+               'last_login', 'contact_number')
     search_fields = ['username', 'users__email']
 
-
 admin.site.register(User, UserAdmin)
-
