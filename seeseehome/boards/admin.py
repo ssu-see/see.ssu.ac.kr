12524c56333e4e3eb8fabcfbf62e1ecc7f7d0f17
wikibootup <wikibootup@gmail.com>
2015-03-30 21:11:54 +0900

    update admin : board inline posts

--- seeseehome/boards/admin.py
+++ seeseehome/boards/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
-from boards.models import *
+from boards.models import Board, Post
 from boards.forms import BoardForm
 
 
-class BoardAdmin(admin.ModelAdmin):
-    form = BoardForm
-    list_display = ('boardname',)
-
-
-class PostAdmin(admin.ModelAdmin):
+class PostInline(admin.TabularInline):
+    model = Post
 
     def has_add_permission(self, request):
         return False
@@ -17,5 +13,14 @@ class PostAdmin(admin.ModelAdmin):
     exclude = ('writer', 'subject', 'content',)
     search_fields = ['subject']
 
+
+class BoardAdmin(admin.ModelAdmin):
+    inlines = [
+        PostInline
+    ]
+
+    form = BoardForm
+    list_display = ('boardname',)
+
+
 admin.site.register(Board, BoardAdmin)
-admin.site.register(Post, PostAdmin)
