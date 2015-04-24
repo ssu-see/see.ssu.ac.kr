501e33f87846e86ad69cdc5ecbbca77bcfbaa989
wikibootup <wikibootup@gmail.com>
2015-04-25 02:55:21 +0900

    refactor users/urls.py : rename url's name

--- seeseehome/users/urls.py
+++ seeseehome/users/urls.py
@@ -1,12 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
-urlpatterns = patterns('',
-                       url(r'^sign-up/', 'users.views.signup', name="signup"),
-                       url(r'^sign-in/', 'users.views.signin', name="signin"),
-                       url(r'^logout/', 'users.views.logout', name="logout"),
-                       url(r'^personalinfo/$', 'users.views.personalinfo', name="personalinfo"),
-                       url(r'^personalinfo/editpersonalinfo/', 'users.views.editpersonalinfo',
-                           name="editpersonalinfo"),
-                       url(r'^personalinfo/editpwd/', 'users.views.editpassword',
-                           name="editpwd"),
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^sign-up/', 'users.views.signup', name="signup"),
+    url(r'^sign-in/', 'users.views.signin', name="signin"),
+    url(r'^logout/', 'users.views.logout', name="logout"),
+    url(r'^mypage/$', 'users.views.personalinfo', name="mypage"),
+    url(r'^mypage/editpersonalinfo/', 'users.views.editpersonalinfo',
+        name="editpersonalinfo"),
+    url(r'^mypage/editpwd/', 'users.views.editpassword',
+        name="editpwd"),
+)
